3cbb9475d4a6e4e116208a46f94e8848d6cb7ca0
reddylavakumar <reddylavakumar2001@gmailcom>
2024-05-24 18:03:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar/Sidebar'
+import Searchbar from './Components/Main/Main'
+
+const App = () => {
+  return (
+    <>
+    <Sidebar/>
+    <Searchbar/>
+    </>
+  )
+}
+
+export default App
