fbd6977a67a6034ba2997789fae5d2d72c61ac50
reddylavakumar <reddylavakumar2001@gmailcom>
2024-05-25 22:21:06 +0530

    Integrated Gemini api into the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar/Sidebar'
 import Main from './Components/Main/Main'
+import ContextProvider from './context/context'
 
 const App = () => {
   return (
     <>
-    
+    <React.StrictMode>
+
+    <ContextProvider>
+
     <Sidebar/>
-    <Main />
+    <Main/>
+    </ContextProvider>
+    </React.StrictMode>
 
     </>
   )
